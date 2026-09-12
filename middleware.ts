814277c7a893db0c436860459959d910b6fe30ff
Hidden Radiology <   GitHub>
2026-09-12 08:08:22 +0300

    temp: disable middleware to go live

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,9 @@
 ﻿import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { LOCALES, DEFAULT_LOCALE } from './lib/config/locales'
 
-export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
-
-  const pathnameHasLocale = LOCALES.some(
-    (locale) =>
-      pathname.startsWith(//) ||
-      pathname === /
-  )
-
-  if (pathnameHasLocale) {
-    return NextResponse.next()
-  }
-
-  const newUrl = request.nextUrl.clone()
-  newUrl.pathname = /
-  return NextResponse.redirect(newUrl)
+export function middleware() {
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|favicon.ico|images|.*\\..*).*)'],
+  matcher: [],
 }
